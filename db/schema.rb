d0c1767acd89d8d64a25f5c99192ce6c04eff734
RichardWaskiewicz <richard.waskiewicz@sage.com>
2015-05-20 11:34:53 -0400

    add user/task reference

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519171649) do
+ActiveRecord::Schema.define(version: 20150520153129) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "context",    limit: 255
+    t.date     "due"
+    t.boolean  "done",       limit: 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
+  end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
