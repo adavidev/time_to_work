e4c911fd75138d8e0231609f37590e43e48fc6f4
RichardWaskiewicz <richard.waskiewicz@sage.com>
2015-05-21 11:42:02 -0400

    redirect user if not logged in

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action {redirect_to new_user_session_path unless current_user}
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   # GET /tasks
